609ab0ea20dfaab803a6225e8d2f5d83890ba904
Juliana Lobo <32516456+xolis@users.noreply.github.com>
2020-12-04 17:26:20 -0300

    Instalação Pacotes

--- SmartSchool.API/Controllers/ProfessorController.cs
+++ SmartSchool.API/Controllers/ProfessorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,49 @@ namespace SmartSchool.API.Controllers
     [ApiController]
     public class ProfessorController : ControllerBase
     {
+        public List<Professor> Professores = new List<Professor>()
+        {
+            new Professor()
+            {
+                Id = 1,
+                Nome = "Matheus",
+                               
+            },
+
+            new Professor()
+            {
+                Id = 2,
+                Nome = "Julia",
+
+            },
+
+            new Professor()
+            {
+                Id = 3,
+                Nome = "Roberto",
+
+            },
+
+            new Professor()
+            {
+                Id = 4,
+                Nome = "Carla",
+
+            },
+        };
+        public ProfessorController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Professores);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //var professor = Professor.FirstOrDefault(p == p.Id == id)
+            return Ok(Professores);
+        }
     }
 }
